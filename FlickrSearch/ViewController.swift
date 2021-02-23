a8a56638f5fb61107abf775ec99a186d13f68104
Alan Hsu <superalanhsu@gmail.com>
2021-02-23 20:15:09 +0800

    Initial Commit

---
+++ FlickrSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrSearch
+//
+//  Created by Alan Hsu on 2021/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
