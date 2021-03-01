322f01f86f9ba78df2e81fe5fa49c55418020ffc
Alan Hsu <superalanhsu@gmail.com>
2021-03-01 19:20:37 +0800

    Changing api key and add readme

--- FlickrSearch/Model/FlickrManager.swift
+++ FlickrSearch/Model/FlickrManager.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct FlickrManager {
-    let flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1804de219111a0a271dacbf6e9bbf97a&format=json&nojsoncallback=1"
+    let flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ee25a6347d43e4acefcf96df57cce47a&format=json&nojsoncallback=1"
     
     func fetchPicture(theType: String, theNumber: String){
         let urlString = "\(flickrURL)&text=\(theType)&per_page=\(theNumber)"
@@ -57,7 +57,6 @@ struct FlickrManager {
 //
 //func fetchData() {
 //
-//    if let url = URL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=dc968a9486be986c2c4cadf22a597027&text=sexy&per_page=20&format=json&nojsoncallback=1") {
 //
 //        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
 //            if let data = data, let searchData = try? JSONDecoder().decode(SearchData.self, from: data){
---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+This project is using MVC as the design pattern
